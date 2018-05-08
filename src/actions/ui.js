260ba9e3fd307364dcb59a86f7f6b31c84c82dda
leonprou <leonp@colu.com>
2018-05-08 18:09:23 +0300

    fix

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -1,4 +1,4 @@
-import {action} from './index'
+import {action} from './utils'
 
 export const setActiveMarker = (coinAddress) => action('ACTIVE_MARKER', {coinAddress})
 //export const closeCommunitySidebar = () => action('CLOSE_COMMUNITY_SIDEBAR', { zoom: 4 })
