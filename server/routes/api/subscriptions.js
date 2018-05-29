7ced0325bb923578a3a3d766608228949016f155
leonprou <leonp@colu.com>
2018-05-29 10:39:54 +0300

    init

--- server/routes/api/subscriptions.js
+++ server/routes/api/subscriptions.js
@@ -3,7 +3,7 @@ const mailchimpConfig = require('../../config').mailchimp
 
 const router = require('express').Router()
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   const mailchimpURL = mailchimpConfig.apiBase + '/lists/' + mailchimpConfig.list + '/members'
   request.post(mailchimpURL, {
     json: true,
@@ -12,7 +12,13 @@ router.post('/', (req, res) => {
       email_type: 'html',
       status: 'subscribed'
     }}).on('response', (response) => {
-    res.send({status: 'ok'})
+      if (response.statusCode < 400) {
+        res.send({status: response.statusCode})
+      } else {
+        next(new Error(`failed to subscribe ${req.body.formData.email}`))
+      }
+  }).on('error', (error) => {
+    next(error)
   })
 })
 
