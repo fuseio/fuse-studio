381044de31d218717bbc94c6825076879bf34ba4
leonprou <leonp@colu.com>
2018-05-31 11:09:14 +0300

    add rouotes tracking

---
+++ client/src/containers/withTracker.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import ReactGA from 'services/ga'
+
+const withTracker = (WrappedComponent, options = {}) => {
+  const trackPage = page => {
+    ReactGA.set({
+      page,
+      ...options
+    })
+    ReactGA.pageview(page)
+  }
+
+  const HOC = class extends Component {
+    componentDidMount () {
+      const page = this.props.location.pathname
+      trackPage(page)
+    }
+
+    componentWillReceiveProps (nextProps) {
+      const currentPage = this.props.location.pathname
+      const nextPage = nextProps.location.pathname
+
+      if (currentPage !== nextPage) {
+        trackPage(nextPage)
+      }
+    }
+
+    render () {
+      return <WrappedComponent {...this.props} />
+    }
+  }
+
+  return HOC
+}
+
+export default withTracker
