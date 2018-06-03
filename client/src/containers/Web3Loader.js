8ee0b242b2324c582e02a78377d4d1618ca6c810
leonprou <leonp@colu.com>
2018-06-03 16:24:31 +0300

    Addding more google analytics

--- client/src/containers/Web3Loader.js
+++ client/src/containers/Web3Loader.js
@@ -3,12 +3,18 @@ import { connect } from 'react-redux'
 
 import {onWeb3Ready} from 'services/web3'
 import {getNetworkType, setReadyStatus} from 'actions/web3'
+import ReactGA from 'services/ga'
 
 class Web3Loader extends React.Component {
 
   componentDidMount() {
       this.props.getNetworkType()
       onWeb3Ready.then(({web3}) => {
+        ReactGA.event({
+          category: 'Metamask',
+          action: 'CLN balance',
+          label: 'Wrong network message'
+        })
         this.props.setReadyStatus(true, web3.eth.defaultAccount)
       })
   }
