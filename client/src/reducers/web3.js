ea8c0cf22a65fd9015eb9b9b28fdff45bf16c063
leonprou <leonp@colu.com>
2018-06-06 13:28:24 +0300

    Handle switch accounts, implementation through polling the current account that web3 uses

--- client/src/reducers/web3.js
+++ client/src/reducers/web3.js
@@ -5,6 +5,8 @@ export default (state = {addresses}, action) => {
   switch (action.type) {
     case web3.SET_READY_STATUS:
       return {...state, ...action.response}
+    case web3.SELECT_ACCOUNT:
+      return {...state, ...action.response}
     case web3.GET_NETWORK_TYPE.SUCCESS:
       return {...state, ...action.response}
     default:
