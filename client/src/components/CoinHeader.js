26be06854ad0c99d7e09cfb06a461d7fd681225d
Elin <elin@colu.com>
2018-07-11 13:42:31 +0300

    lint

--- client/src/components/CoinHeader.js
+++ client/src/components/CoinHeader.js
@@ -3,20 +3,20 @@ import classNames from 'classnames'
 import { formatAmountReal, formatMoney } from 'services/global'
 
 export default class CoinHeader extends Component {
-	render() {
-		const formattedPrice = this.props.price || this.props.price === 0 ? formatMoney(formatAmountReal(this.props.price, 18), 4, '.', ',') : 'loading'
-		return (
-			<div className="coin-header">
-				<img src={this.props.coinImage} />
-				<div className="coin-details">
-					<h1>{this.props.name}</h1>
-					<div className="separator"/>
-					<div className="price-wrapper">
-						<h2>Current price:</h2>
-						<p>{formattedPrice + ' CLN'}</p>
-					</div>
-				</div>
-			</div>
-		)
-	}
+  render() {
+    const formattedPrice = this.props.price || this.props.price === 0 ? formatMoney(formatAmountReal(this.props.price, 18), 4, '.', ',') : 'loading'
+    return (
+      <div className="coin-header">
+        <img src={this.props.coinImage} />
+        <div className="coin-details">
+          <h1>{this.props.name}</h1>
+          <div className="separator"/>
+          <div className="price-wrapper">
+            <h2>Current price:</h2>
+            <p>{formattedPrice + ' CLN'}</p>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
--- client/src/components/TextInput.js
+++ client/src/components/TextInput.js
@@ -4,16 +4,16 @@ import classnames from 'classnames'
 import Select from './Select'
 
 const options = [
-	{ value: 'Buy & Sell CLN / CC', label: 'Buy & Sell CLN / CC'},
-	{ value: 'Issue new coin', label: 'Issue new coin' },
-	{ value: 'Partnerships', label: 'Partnerships' },
-	{ value: 'Technical Issue', label: 'Technical Issue' },
-	{ value: 'General Inquiry', label: 'General Inquiry' }
+  { value: 'Buy & Sell CLN / CC', label: 'Buy & Sell CLN / CC'},
+  { value: 'Issue new coin', label: 'Issue new coin' },
+  { value: 'Partnerships', label: 'Partnerships' },
+  { value: 'Technical Issue', label: 'Technical Issue' },
+  { value: 'General Inquiry', label: 'General Inquiry' }
 ]
 
 const InputFeedback = ({ error }) =>
   error ? (
-    <div className="input-feedback">{error}</div>
+  <div className="input-feedback">{error}</div>
   ) : null;
 
 const Label = ({
@@ -23,80 +23,80 @@ const Label = ({
   ...props
 }) => {
   return (
-    <label className="label" {...props}>
-      {children}
-    </label>
+  <label className="label" {...props}>
+    {children}
+  </label>
   );
 }
 
 const TextInput = ({
-	fieldType,
-	type,
-	id,
-	label,
-	error,
-	value,
-	onChange,
-	className,
-	setFieldValue,
-	children,
-	...props
+  fieldType,
+  type,
+  id,
+  label,
+  error,
+  value,
+  onChange,
+  className,
+  setFieldValue,
+  children,
+  ...props
 }) => {
-	const classes = classnames(
-		'contact-field-wrapper',
-		{
-			'checkbox': fieldType === 'checkbox',
-			'error': !!error,
-		},
-		className
-	)
-	let field
-	switch(fieldType) {
-		case "textarea": {
-			field = <textarea
-					id={id}
-					className="text-input"
-					type={type}
-					value={value}
-					onChange={onChange}
-					style={{height: isMobile ? 'auto' : '52px'}}
-					{...props}
-				/>
-			break;
-		}
-		case "select": {
-			field = <Select id={id} value={value} setFieldValue={setFieldValue} options={options} {...props}/>
-			break;
-		}
-		case "checkbox": {
-			field = <input
-					id={id}
-					type={type}
-					checked={value}
-					onChange={onChange}
-					{...props}/>
-			break;
-		}
-		default: {
-			field = <input
-					id={id}
-					className="text-input"
-					type={type}
-					value={value}
-					onChange={onChange}
-					{...props}/>
-			break;
-		}
-	}
-	return (
-		<div className={classes}>
-			<Label htmlFor={id} error={error}>
-				{label}
-			</Label>
-			{field}
-			<InputFeedback error={error} />
-		</div>
-	)
+  const classes = classnames(
+    'contact-field-wrapper',
+    {
+      'checkbox': fieldType === 'checkbox',
+      'error': !!error,
+    },
+    className
+  )
+  let field
+  switch(fieldType) {
+    case "textarea": {
+      field = <textarea
+          id={id}
+          className="text-input"
+          type={type}
+          value={value}
+          onChange={onChange}
+          style={{height: isMobile ? 'auto' : '52px'}}
+          {...props}
+        />
+      break;
+    }
+    case "select": {
+      field = <Select id={id} value={value} setFieldValue={setFieldValue} options={options} {...props}/>
+      break;
+    }
+    case "checkbox": {
+      field = <input
+          id={id}
+          type={type}
+          checked={value}
+          onChange={onChange}
+          {...props}/>
+      break;
+    }
+    default: {
+      field = <input
+          id={id}
+          className="text-input"
+          type={type}
+          value={value}
+          onChange={onChange}
+          {...props}/>
+      break;
+    }
+  }
+  return (
+    <div className={classes}>
+      <Label htmlFor={id} error={error}>
+        {label}
+      </Label>
+      {field}
+      <InputFeedback error={error} />
+    </div>
+  )
 }
 
 export default TextInput
