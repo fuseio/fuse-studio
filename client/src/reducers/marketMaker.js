2c83da244f002ddad5cccd112a86319c800266ca
leonprou <leonp@colu.com>
2018-07-26 11:49:32 +0300

    fix

--- client/src/reducers/marketMaker.js
+++ client/src/reducers/marketMaker.js
@@ -5,13 +5,13 @@ import {filterSuccessActions} from 'utils/actions'
 const successActions = filterSuccessActions(actions)
 successActions[actions.CHANGE.PENDING] = actions.CHANGE
 
-const tokenActions = new Set(
+const tokenActions = new Set([
   actions.GET_CURRENT_PRICE.SUCCESS,
   actions.CLN_RESERVE.SUCCESS,
   actions.CC_RESERVE.SUCCESS,
   actions.IS_OPEN_FOR_PUBLIC.SUCCESS,
   actions.FETCH_MARKET_MAKER_DATA.SUCCESS
-)
+])
 
 export default (state = {}, action) => {
   if (tokenActions.has(action.type)) {
