352bb7fa8f7ccb657b4ce483b53f157a20748914
Leon Prouger <leonprou@gmail.com>
2018-08-22 15:39:44 +0300

    change precision away from zero

--- client/src/utils/format.js
+++ client/src/utils/format.js
@@ -3,5 +3,5 @@ import {BigNumber} from 'bignumber.js'
 export const ROUND_PRECISION = 4
 export const MAX_PRECISION = 18
 
-export const formatWei = (value) => new BigNumber(value).div(1e18).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_DOWN)
-export const formatEther = (value) => new BigNumber(value).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_DOWN)
+export const formatWei = (value) => new BigNumber(value).div(1e18).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_UP)
+export const formatEther = (value) => new BigNumber(value).toFormat(ROUND_PRECISION, BigNumber.ROUND_HALF_UP)
