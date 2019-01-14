020acb3c4f65d4d5b14981d3c39f4a4eb824621a
Kukushkina <inessakukushkina@gmail.com>
2019-01-14 13:07:35 +0300

    personal sidebar get tokens (#171)

--- client/src/actions/accounts.js
+++ client/src/actions/accounts.js
@@ -12,4 +12,6 @@ export const balanceOfCln = (accountAddress) => action(BALANCE_OF_CLN.REQUEST, {
 
 export const fetchTokens = (accountAddress) => action(FETCH_TOKENS.REQUEST, {accountAddress})
 export const fetchBalances = (tokens, accountAddress) => action(FETCH_BALANCES.REQUEST, {accountAddress, tokens})
+export const fetchTokensWithBalances = (accountAddress) => action(FETCH_TOKENS_WITH_BALANCES.REQUEST, {accountAddress})
+
 export const setUserInformation = (user) => action(SET_USER_INFORMATION.REQUEST, {user})
