93f5103e218504a1187a4434562374d4d937daab
Leon Prouger <leonprou@gmail.com>
2019-04-24 13:59:51 +0300

    add withMetadata query param for businesses (#244)
    * add withMetadata query param for businesses
    
    * improve result

--- server/src/routes/api/businesses.js
+++ server/src/routes/api/businesses.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const mongoose = require('mongoose')
 const Business = mongoose.model('Business')
 const paginate = require('express-paginate')
+const {getMetadata} = require('@utils/metadata')
 
 const getQueryFilter = (req) => {
   const {listAddress} = req.params
@@ -12,14 +13,22 @@ const getQueryFilter = (req) => {
     return {listAddress}
   }
 }
+
 router.get('/:listAddress', async (req, res, next) => {
   const queryFilter = getQueryFilter(req)
-  const [ results, itemCount ] = await Promise.all([
+  const {withMetadata} = req.query
+
+  let [ results, itemCount ] = await Promise.all([
     Business.find(queryFilter).sort({name: 1}).limit(req.query.limit).skip(req.skip).lean(),
     Business.estimatedDocumentCount({})
   ])
 
   const pageCount = Math.ceil(itemCount / req.query.limit)
+
+  if (withMetadata) {
+    const metadatas = await Promise.all(results.map(result => getMetadata(result.hash).catch(console.error)))
+    results = results.map((result, index) => ({...result, metadata: metadatas[index] && metadatas[index].data}))
+  }
 
   res.json({
     object: 'list',
@@ -30,9 +39,15 @@ router.get('/:listAddress', async (req, res, next) => {
 
 router.get('/:listAddress/:hash', async (req, res, next) => {
   const {listAddress, hash} = req.params
-
+  const {withMetadata} = req.query
   const business = await Business.findOne({ listAddress, hash }).lean()
-  return res.json({ data: business })
+
+  if (withMetadata) {
+    const metadata = await getMetadata(business.hash).catch(console.error)
+    return res.json({data: {...business, metadata: metadata && metadata.data}})
+  } else {
+    return res.json({ data: business })
+  }
 })
 
 module.exports = router
