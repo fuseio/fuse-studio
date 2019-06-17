3cb84336665bbb74095d012ea6070a0a28fccb6f
Lior <liorag@colu.com>
2019-06-17 19:03:07 +0300

    added faq

--- dapp/src/components/home/components/Faq/index.jsx
+++ dapp/src/components/home/components/Faq/index.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import FAQ from 'constants/faq'
 
 export default (props) => {
   return (
-    [1, 2, 3].map((val, index) => {
+    FAQ.map(({ question, answer }, index) => {
       return (
         <div className='faq' key={index}>
-          <h4 className='faq__title'>How is FAQ pronounced?</h4>
-          <p className='faq__content'>Luram ipsum bla bla bla bla bla bla bla bla bla blabla bla…</p>
+          <h4 className='faq__title'>{question}</h4>
+          <p className='faq__content'>{answer}</p>
           <span className='faq__read'>Read more</span>
           <br />
         </div>
