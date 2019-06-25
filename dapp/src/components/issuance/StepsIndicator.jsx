5ecd7bd61c5625ee51396c78eb3b232c71583b0e
Lior <liorag@colu.com>
2019-06-25 15:59:02 +0300

    refactor

--- dapp/src/components/issuance/StepsIndicator.jsx
+++ dapp/src/components/issuance/StepsIndicator.jsx
@@ -3,10 +3,9 @@ import classNames from 'classnames'
 
 const StepsIndicator = ({ steps, activeStep, network }) => {
   return steps.map((item, key) => {
-    const stepsClassStyle = classNames('cell large-2 medium-2 small-4', {
-      [`step-${network}`]: true,
-      [`step-${network}--active`]: key === activeStep,
-      [`step-${network}--done`]: key < activeStep
+    const stepsClassStyle = classNames('cell large-2 medium-2 small-4 step', {
+      [`step--active`]: key === activeStep,
+      [`step--done`]: key < activeStep
     })
     return (
       <div key={key} className={stepsClassStyle} />
