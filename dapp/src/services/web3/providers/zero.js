3aea268dbf45b5a9bce23ef063c0a7265f0d9b0f
Leon Prouger <leonprou@gmail.com>
2019-06-30 17:42:32 +0300

    add provider by private key

--- dapp/src/services/web3/providers/zero.js
+++ dapp/src/services/web3/providers/zero.js
@@ -3,16 +3,6 @@ import WalletSubprovider from 'ethereumjs-wallet/provider-engine'
 import ZeroClientProvider from 'web3-provider-engine/zero'
 import { toBuffer } from 'ethereumjs-util'
 
-// const pk = generate()._privKey
-// console.log(isValidPrivate(pk))
-
-// const wallet = fromPrivateKey(pk)
-// const wallerProvider = new WalletSubprovider(wallet)
-// const provider = new ZeroClientProvider({
-//   rpcUrl: CONFIG.web3.fuseProvider,
-//   ...wallerProvider
-// })
-
 export const getProvider = (opts = {}) => {
   const pk = opts.pk ? toBuffer(opts.pk) : generate()._privKey
 
@@ -20,14 +10,17 @@ export const getProvider = (opts = {}) => {
   const walletProvider = new WalletSubprovider(wallet)
   const provider = new ZeroClientProvider({
     rpcUrl: CONFIG.web3.fuseProvider,
-    ...walletProvider
+    ...walletProvider,
+    engineParams: {
+      useSkipCache: false
+    }
   })
 
   if (!window.ethereum) {
     window.ethereum = provider
     window.ethereum.enable = () =>
       new Promise((resolve, reject) => {
-        provider.sendAsync({ method: 'eth_accounts' }, (error, response) => {
+        provider.sendAsync({ method: 'eth_accounts', params: [] }, (error, response) => {
           if (error) {
             reject(error)
           } else {
