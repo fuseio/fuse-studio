02bfe4d88930af05ed95953b83573d80431c49b8
Lior <liorag@colu.com>
2019-07-15 14:57:01 +0300

    linter

--- dapp/src/components/common/JoinCommunity/index.jsx
+++ dapp/src/components/common/JoinCommunity/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import { withNetwork, withAccount, withBox } from 'containers/Web3'
 import { connect } from 'react-redux'
 import { joinCommunity } from 'actions/communityEntities'
