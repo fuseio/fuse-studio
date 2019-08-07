7de706f24eea83037483309d7f57b51c8bbeda96
Lior <liorag@colu.com>
2019-08-07 10:26:37 +0300

    hope this fix the bug in the tables

--- dapp/src/components/dashboard/components/HeaderRow/index.jsx
+++ dapp/src/components/dashboard/components/HeaderRow/index.jsx
@@ -5,7 +5,7 @@ export default ({
   headerGroup
 }) => {
   return (
-    <div {...headerGroup.getRowProps({ className: 'table__header grid-x align-middle align-spaced' })}>
+    <div {...headerGroup.getHeaderGroupProps({ className: 'table__header grid-x align-middle align-spaced' })}>
       {headerGroup.headers.map(column => {
         const { id } = column
         const className = id === 'checkbox' || id === 'dropdown' ? `table__body__cell cell small-2` : `table__body__cell cell small-${Math.ceil(24 / headerGroup.headers.length)}`
