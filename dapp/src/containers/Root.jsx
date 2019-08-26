cd83d00f5bb0fab89694b9b644755e3e88ea47b0
Lior <liorag@colu.com>
2019-08-26 15:20:18 +0300

    new analytics added

--- dapp/src/containers/Root.jsx
+++ dapp/src/containers/Root.jsx
@@ -8,7 +8,7 @@ import DashboardLayout from 'components/dashboard/containers/MainDashboard'
 import MobileProvider from 'containers/MobileProvider'
 import SignInProvider from 'containers/SignInProvider'
 import EntityProfile from 'components/dashboard/EntityProfile'
-import withTracker from 'containers/withTracker'
+// import withTracker from 'containers/withTracker'
 import Web3, { withNetwork } from 'containers/Web3'
 import Layout from 'components/common/Layout'
 import HomePage from 'components/home/pages/HomePage'
@@ -27,11 +27,11 @@ export default class Root extends Component {
               {!history.location.pathname.includes('/join') && !history.location.pathname.includes('/sign')
                 ? <Web3 /> : undefined}
               <Switch>
-                <Route exact path='/' component={withTracker(withNetwork(HomePage))} />
-                <Route path='/view/issuance' component={withTracker(withNetwork(IssuanceWizard))} />
-                <Route path='/view/communities' component={withTracker(withNetwork(Oven))} />
-                <Route path='/view/community/:address' component={withTracker(withNetwork(DashboardLayout))} />
-                <Route path='/view/directory/:communityAddress/:account' component={withTracker(withNetwork(EntityProfile))} />
+                <Route exact path='/' component={withNetwork(HomePage)} />
+                <Route path='/view/issuance' component={withNetwork(IssuanceWizard)} />
+                <Route path='/view/communities' component={withNetwork(Oven)} />
+                <Route path='/view/community/:address' component={withNetwork(DashboardLayout)} />
+                <Route path='/view/directory/:communityAddress/:account' component={withNetwork(EntityProfile)} />
                 <Route path='/view/sign/:isMobileApp?' component={SignInProvider} />
                 <Route path='/view/join/:address' component={MobileProvider} />
               </Switch>
