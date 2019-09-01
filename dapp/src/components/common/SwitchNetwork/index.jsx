11c62bc481dd14cc72f63dc9b792a080fe06ed26
Lior <liorag@colu.com>
2019-09-01 17:25:42 +0300

    more improvements

--- dapp/src/components/common/SwitchNetwork/index.jsx
+++ dapp/src/components/common/SwitchNetwork/index.jsx
@@ -4,10 +4,12 @@ import ChangeNetwork from 'images/change_network.png'
 const SwitchNetwork = () => {
   return (
     <div className='switch__network'>
-      <h3 className='switch__network__title'>Switch to fuse chain</h3>
-      <div className='switch__network__content'>In order to use Community 123 you need to switch to the Fuse chain
-        To add Fuse chain on metamask, please add a custom RPC and enter https://rpc.fusenet.io</div>
+      <h3 className='switch__network__title'>Switch to Fuse chain to use plug-ins</h3>
+      <div className='switch__network__content switch__network__content--space'>
+        You need to switch to the Fuse chain on Metamask: <br />
+      </div>
       <div className='switch__network__image'><img src={ChangeNetwork} /></div>
+      <div className='switch__network__content'><a target='_blank' href='https://docs.fusenet.io/the-fuse-studio/how-to-add-fuse-to-your-metamask'>Click here</a> to learn how to add Fuse to your Metamask</div>
     </div>
   )
 }
