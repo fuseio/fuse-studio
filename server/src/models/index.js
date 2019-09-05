d54b9725ec42a6da02a183a6bc3462d144d69de7
Lior Agnin <Lior.Agnin@gmail.com>
2019-09-05 10:33:46 +0300

    removed imports (#19)
    * removed imports
    
    * remove 2

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -3,8 +3,6 @@ module.exports = (mongoose) => {
   mongoose.event = mongoose.event || require('./Event')(mongoose)
   mongoose.token = mongoose.token || require('./Token')(mongoose)
   mongoose.user = mongoose.user || require('./User')(mongoose)
-  mongoose.business = mongoose.business || require('./Business')(mongoose)
-  mongoose.businessList = mongoose.businessList || require('./BusinessList')(mongoose)
   mongoose.bridge = mongoose.bridge || require('./Bridge')(mongoose)
   mongoose.account = mongoose.account || require('./Account')(mongoose)
   mongoose.community = mongoose.community || require('./Community')(mongoose)
