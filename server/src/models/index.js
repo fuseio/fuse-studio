e4c7bd8f72bd3a811b1b3d09851b7910e6b721ba
Lior <liorag@colu.com>
2019-09-05 10:30:04 +0300

    removed imports

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -4,7 +4,6 @@ module.exports = (mongoose) => {
   mongoose.token = mongoose.token || require('./Token')(mongoose)
   mongoose.user = mongoose.user || require('./User')(mongoose)
   mongoose.business = mongoose.business || require('./Business')(mongoose)
-  mongoose.businessList = mongoose.businessList || require('./BusinessList')(mongoose)
   mongoose.bridge = mongoose.bridge || require('./Bridge')(mongoose)
   mongoose.account = mongoose.account || require('./Account')(mongoose)
   mongoose.community = mongoose.community || require('./Community')(mongoose)
