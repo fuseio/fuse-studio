bc77cc9f211a3c3366208630803de56423ffbded
Lior <liorag@colu.com>
2019-09-19 13:20:11 +0300

    improved

--- dapp/src/components/common/Community/index.jsx
+++ dapp/src/components/common/Community/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { memo } from 'react'
 import PropTypes from 'prop-types'
 import identity from 'lodash/identity'
 import { formatWei } from 'utils/format'
@@ -6,48 +6,54 @@ import CommunityLogo from 'components/common/CommunityLogo'
 import { isDaiToken } from 'constants/existingTokens'
 import isEmpty from 'lodash/isEmpty'
 
-export default class Community extends Component {
-  handleClick = () => {
-    const { token } = this.props
-    if (token && token.communityAddress) {
-      this.props.showDashboard(token.communityAddress)
+const Community = memo(({
+  token,
+  showDashboard,
+  networkType,
+  metadata
+}) => {
+  const {
+    name,
+    totalSupply,
+    communityAddress
+  } = token
+
+  const handleClick = () => {
+    if (communityAddress) {
+      showDashboard(communityAddress)
     }
   }
 
-  render () {
-    const {
-      token,
-      networkType,
-      metadata
-    } = this.props
-    const {
-      name,
-      totalSupply
-    } = token
-
-    const { image } = metadata
-    return (
-      <div className='community' onClick={this.handleClick}>
-        <div className='community__logo'>
-          <CommunityLogo
-            isDaiToken={isDaiToken(networkType, token)}
-            token={token}
-            networkType={networkType}
-            imageUrl={!isEmpty(image) ? `${CONFIG.ipfsProxy.urlBase}/image/${image}` : null}
-            metadata={metadata}
-          />
-        </div>
-        <div className='community__content'>
-          <h3 className='community__content__title'>{name}</h3>
-          <p className='community__content__members'>
-            Total Supply
-            <span>{formatWei(totalSupply, 0)}</span>
-          </p>
-        </div>
+  const { image } = metadata
+  return (
+    <div className='community' onClick={handleClick}>
+      <div className='community__logo'>
+        <CommunityLogo
+          isDaiToken={isDaiToken(networkType, token)}
+          token={token}
+          networkType={networkType}
+          imageUrl={!isEmpty(image) ? `${CONFIG.ipfsProxy.urlBase}/image/${image}` : null}
+          metadata={metadata}
+        />
       </div>
-    )
+      <div className='community__content'>
+        <h3 className='community__content__title'>{name}</h3>
+        <p className='community__content__members'>
+          Total Supply
+          <span>{formatWei(totalSupply, 0)}</span>
+        </p>
+      </div>
+    </div>
+  )
+}, (prevProps, nextProps) => {
+  if (prevProps.token !== nextProps.token) {
+    return false
+  } else if (prevProps.metadata !== nextProps.metadata) {
+    return false
   }
-}
+
+  return true
+})
 
 Community.defaultProps = {
   token: {},
@@ -56,7 +62,8 @@ Community.defaultProps = {
 }
 
 Community.propTypes = {
-  coinWrapperClassName: PropTypes.string,
   token: PropTypes.object,
   metadata: PropTypes.object
 }
+
+export default Community
