a8a224c97866fa30037b7ecc36ed6f1fab211244
Lior <liorag@colu.com>
2019-10-17 19:56:56 +0300

    linter

--- dapp/src/components/common/ProfileDropDown/index.jsx
+++ dapp/src/components/common/ProfileDropDown/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect } from 'react'
 import { connect, useSelector } from 'react-redux'
 import FontAwesome from 'react-fontawesome'
 import { formatAddress, formatWei } from 'utils/format'
