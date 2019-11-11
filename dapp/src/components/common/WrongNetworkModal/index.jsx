099a1628002893f0410eefc075d9917f824fc7e7
Lior <liorag@colu.com>
2019-11-11 17:19:45 +0200

    can't see community if on wrong network

--- dapp/src/components/common/WrongNetworkModal/index.jsx
+++ dapp/src/components/common/WrongNetworkModal/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import capitalize from 'lodash/capitalize'
 import PropTypes from 'prop-types'
 import GenericModal from 'components/dashboard/modals/GenericModal'
 import wrongNetwork from 'images/wrong_network.svg'
 
-const Networks = ({ networks }) => networks.map(network => <strong key={network} className='capitalize' style={{ color: '#4a687b' }}>{network} Network</strong>)
+const Networks = ({ networks }) => networks.map(network => <strong key={network} className='capitalize' style={{ color: '#4a687b' }}>{capitalize(network)} Network</strong>)
   .reduce((prev, curr) => [prev, ' or ', curr])
 
 class WrongNetworkModal extends React.Component {
@@ -15,11 +16,11 @@ class WrongNetworkModal extends React.Component {
   }
 
   render () {
+    const { body } = this.props
     const content = {
       image: wrongNetwork,
       title: 'Wrong network',
-      text: 'Hi there, seems that you\'re on the wrong network.',
-      body: <p>Please open Metamask and switch to the <Networks networks={this.props.supportedNetworks} /></p>,
+      body: body || <p>Please open Metamask and switch to the <Networks networks={this.props.supportedNetworks} /></p>,
       buttonText: 'Ok'
     }
     return (
@@ -34,7 +35,8 @@ class WrongNetworkModal extends React.Component {
 }
 
 WrongNetworkModal.defaultProps = {
-  supportedNetworks: CONFIG.web3.supportedNetworks
+  supportedNetworks: CONFIG.web3.supportedNetworks,
+  body: 'Hi there, seems that you\'re on the wrong network.'
 }
 
 WrongNetworkModal.propTypes = {
