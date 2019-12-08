f697d15c44f5f208307f207ca3e8d285bf8ce2d8
Lior Rabin <lior9586@gmail.com>
2019-12-08 13:59:49 +0200

    Feature/relay - transfer (#83)
    * TransferManager ABI
    
    * auth required on relay api endpoint
    
    * lint fixes

--- server/src/routes/api/v2/relay.js
+++ server/src/routes/api/v2/relay.js
@@ -1,7 +1,8 @@
 const router = require('express').Router()
 const { agenda } = require('@services/agenda')
+const auth = require('@routes/auth')
 
-router.post('/', async (req, res) => {
+router.post('/', auth.required, async (req, res) => {
   await agenda.now('relay', req.body)
   return res.json({ response: 'ok' })
 })
--- server/src/tasks/relay.js
+++ server/src/tasks/relay.js
@@ -4,7 +4,7 @@ const { createNetwork } = require('@utils/web3')
 const homeAddresses = config.get('network.home.addresses')
 
 const relay = withAccount(async (account, { walletAddress, methodData, nonce, gasPrice, gasLimit, signature, walletModule }) => {
-  const { web3, createContract, createMethod, send } = createNetwork('home', account)
+  const { createContract, createMethod, send } = createNetwork('home', account)
   const ABI = require(`@constants/abi/${walletModule}`)
   const contract = createContract(ABI, homeAddresses.walletModules[walletModule])
   const method = createMethod(contract, 'execute', walletAddress, methodData, nonce, signature, gasPrice, gasLimit)
