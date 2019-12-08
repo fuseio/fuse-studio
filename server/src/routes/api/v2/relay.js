461346e707644485a1da7b7913f54f904bf5460a
Lior Rabin <lior@fusenet.io>
2019-12-08 13:56:47 +0200

    lint fixes

--- server/src/routes/api/v2/relay.js
+++ server/src/routes/api/v2/relay.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const { agenda } = require('@services/agenda')
+const auth = require('@routes/auth')
 
 router.post('/', auth.required, async (req, res) => {
   await agenda.now('relay', req.body)
