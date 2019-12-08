f023a6338642ce2d10e8d8beffd6f9dcc36aca45
Lior Rabin <lior@fusenet.io>
2019-12-08 11:43:17 +0200

    get wallet module to execute on from request

--- server/src/tasks/relay.js
+++ server/src/tasks/relay.js
@@ -1,12 +1,12 @@
 const config = require('config')
 const { withAccount } = require('@utils/account')
 const { createNetwork } = require('@utils/web3')
-const CommunityManager = require(`@constants/abi/CommunityManager`)
 const homeAddresses = config.get('network.home.addresses')
 
-const relay = withAccount(async (account, { walletAddress, methodData, nonce, gasPrice, gasLimit, signature }) => {
+const relay = withAccount(async (account, { walletAddress, methodData, nonce, gasPrice, gasLimit, signature, walletModule }) => {
   const { web3, createContract, createMethod, send } = createNetwork('home', account)
-  const contract = createContract(CommunityManager, homeAddresses.walletModules.CommunityManager)
+  const ABI = require(`@constants/abi/${walletModule}`)
+  const contract = createContract(ABI, homeAddresses.walletModules[walletModule])
   const method = createMethod(contract, 'execute', walletAddress, methodData, nonce, signature, gasPrice, gasLimit)
   const receipt = await send(method, {
     from: account.address
