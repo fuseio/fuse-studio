b801416852a0bdbaa3171691320ad0f1f8ab17f8
Leon Prouger <leonprou@gmail.com>
2019-12-17 12:04:10 +0200

    feature funder

--- server/src/tasks/relay.js
+++ server/src/tasks/relay.js
@@ -2,9 +2,10 @@ const config = require('config')
 const { withAccount } = require('@utils/account')
 const { createNetwork } = require('@utils/web3')
 const homeAddresses = config.get('network.home.addresses')
+const request = require('request-promise-native')
 
-const relay = withAccount(async (account, { walletAddress, methodData, nonce, gasPrice, gasLimit, signature, walletModule }, job) => {
-  const { createContract, createMethod, send } = createNetwork('home', account)
+const relay = withAccount(async (account, { walletAddress, methodData, nonce, gasPrice, gasLimit, signature, walletModule, args }, job) => {
+  const { web3, createContract, createMethod, send } = createNetwork('home', account)
   const ABI = require(`@constants/abi/${walletModule}`)
   const contract = createContract(ABI, homeAddresses.walletModules[walletModule])
   const method = createMethod(contract, 'execute', walletAddress, methodData, nonce, signature, gasPrice, gasLimit)
@@ -16,6 +17,24 @@ const relay = withAccount(async (account, { walletAddress, methodData, nonce, ga
       job.save()
     }
   })
+  console.log({ methodData })
+  console.log({ args })
+  const { success, wallet, signedHash } = receipt.events.TransactionExecuted.returnValues
+  if (success) {
+    console.log(`Relay transaction executed successfully from wallet: ${wallet}, signedHash: ${signedHash}`)
+    if (walletModule === 'CommunityManager') {
+      console.log(`Requesting token funding for walltet $wallet`)
+      const methodAbi = getMethod()
+      const parsedArguments = web3.eth.abi.decodeParameters(methodAbi.inputs)
+      const communityAddress = parsedArguments._community
+      // request.post(`${config.get('funder.urlBase')}fund/token`, {
+      //   json: true,
+      //   body: { accountAddress: walletAddress, tokenAddress, networkType: config.get('network.foreign.name') }
+      // })
+    }
+  } else {
+    console.error(`Relay transaction failed from wallet: ${wallet}, signedHash: ${signedHash}`)
+  }
   return receipt
 })
 
