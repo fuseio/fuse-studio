4e500defab83524ed642c4a5784adb41938eeaaf
Lior Rabin <lior@fuse.io>
2019-12-18 14:20:26 +0200

    removed commented logs

--- server/src/tasks/relay.js
+++ server/src/tasks/relay.js
@@ -8,12 +8,8 @@ const request = require('request-promise-native')
 
 function getParamsFromMethodData (web3, abi, methodName, methodData) {
   const methodABI = abi.filter(obj => obj.name === methodName)[0]
-  // console.log({ methodABI })
   const methodSig = web3.eth.abi.encodeFunctionSignature(methodABI)
-  // console.log({ methodSig })
-  // console.log({ inputs: methodABI.inputs })
   const params = web3.eth.abi.decodeParameters(methodABI.inputs, `0x${methodData.replace(methodSig, '')}`)
-  // console.log({ params })
   return params
 }
 
@@ -30,9 +26,7 @@ const relay = withAccount(async (account, { walletAddress, methodData, nonce, ga
       job.save()
     }
   })
-  // console.log({ methodData })
   const { success, wallet, signedHash } = receipt.events.TransactionExecuted.returnValues
-  // console.log({ success, wallet, signedHash })
   if (success) {
     console.log(`Relay transaction executed successfully from wallet: ${wallet}, signedHash: ${signedHash}`)
     if (walletModule === 'CommunityManager') {
@@ -40,9 +34,7 @@ const relay = withAccount(async (account, { walletAddress, methodData, nonce, ga
         console.log(`Requesting token funding for wallet: ${wallet}`)
         const params = getParamsFromMethodData(web3, ABI, 'joinCommunity', methodData)
         const query = `{tokens(where: {communityAddress: "${params._community}"}) {address, communityAddress, originNetwork}}`
-        // console.log({ query })
         const { tokens } = await graphClient.request(query)
-        // console.log({ tokens })
         const tokenAddress = web3.utils.toChecksumAddress(tokens[0].address)
         const originNetwork = tokens[0].originNetwork
         request.post(`${config.get('funder.urlBase')}fund/token`, {
