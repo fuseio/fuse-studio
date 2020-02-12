acee7daae86bd6bc645e873dfa1f1f683a874013
Lior Rabin <lior@fuse.io>
2020-02-12 17:44:44 +0200

    bonus to inviter using funder

--- server/src/tasks/wallet.js
+++ server/src/tasks/wallet.js
@@ -10,9 +10,12 @@ const UserWallet = mongoose.model('UserWallet')
 const Contact = mongoose.model('Contact')
 const branch = require('@utils/branch')
 const twilio = require('@utils/twilio')
+const { GraphQLClient } = require('graphql-request')
+const graphClient = new GraphQLClient(config.get('graph.url'))
+const request = require('request-promise-native')
 
-const createWallet = withAccount(async (account, { owner, communityAddress, phoneNumber, ens = '', name, amount, symbol }, job) => {
-  const { createContract, createMethod, send } = createNetwork('home', account)
+const createWallet = withAccount(async (account, { owner, communityAddress, phoneNumber, ens = '', name, amount, symbol, bonusInfo }, job) => {
+  const { web3, createContract, createMethod, send } = createNetwork('home', account)
   const walletFactory = createContract(WalletFactoryABI, homeAddresses.WalletFactory)
   const method = createMethod(walletFactory, 'createWallet', owner, Object.values(homeAddresses.walletModules), ens)
 
@@ -47,7 +50,27 @@ const createWallet = withAccount(async (account, { owner, communityAddress, phon
 
     const inviteTxt = `${name} sent you ${amount} ${symbol}! Click here to redeem:\n${url}`
     twilio.createMessage({ to: phoneNumber, body: inviteTxt })
+
+    if (bonusInfo) {
+      try {
+        bonusInfo.bonusId = walletAddress
+        console.log(`Requesting token bonus for wallet: ${bonusInfo.receiver}`)
+        const query = `{tokens(where: {communityAddress: "${communityAddress}"}) {address, communityAddress, originNetwork}}`
+        const { tokens } = await graphClient.request(query)
+        const tokenAddress = web3.utils.toChecksumAddress(tokens[0].address)
+        const originNetwork = tokens[0].originNetwork
+        request.post(`${config.get('funder.urlBase')}bonus/token`, {
+          json: true,
+          body: { accountAddress: bonusInfo.receiver, tokenAddress, originNetwork, bonusInfo }
+        })
+      } catch (e) {
+        console.log(`Error on token bonus for wallet: ${bonusInfo.receiver}`, e)
+      }
+    }
   }
+
+  // TODO save invite in new DB model
+
   return receipt
 })
 
