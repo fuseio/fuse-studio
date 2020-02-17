0a5cc4025fad8ed17389172472c2188a0f3146bd
Leon Prouger <leonprou@gmail.com>
2020-02-17 16:31:11 +0200

    delete debug

--- dapp/src/components/dashboard/components/Table/index.jsx
+++ dapp/src/components/dashboard/components/Table/index.jsx
@@ -90,7 +90,6 @@ const MyTable = ({
           <select
             defaultValue={pageIndex + 1}
             onChange={e => {
-              debugger
               const page = e.target.value ? Number(e.target.value) - 1 : 0
               gotoPage(page)
             }}
