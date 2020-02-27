172d5c66363f0952780bd482b2c8ab84c4286ecd
Leon Prouger <leonprou@gmail.com>
2020-02-27 11:05:45 +0200

    fixing senderId (#220)

--- server/config/default.js
+++ server/config/default.js
@@ -182,7 +182,7 @@ module.exports = {
   aws: {
     sns: {
       region: 'eu-west-1',
-      senderId: 'Digital Wallet',
+      senderId: 'DigitalWallet',
       smsType: 'Promotional'
     }
   },
