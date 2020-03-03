8c22077fb9af2a805b4593d98dfb8b5479d690f4
Leon Prouger <leonprou@gmail.com>
2020-03-03 15:56:00 +0200

    Change white label wallet texts (#226)
    * Change white label wallet texts
    
    * change template
    
    * change template text

--- dapp/src/components/dashboard/pages/WhiteLabelWallet/index.jsx
+++ dapp/src/components/dashboard/pages/WhiteLabelWallet/index.jsx
@@ -19,19 +19,24 @@ const WhiteLabelWallet = ({ value, communityAddress }) => {
       </div>
       <div className='qr-code__wrapper'>
         <div className='qr-code'>
-          <ol className='qr-code__steps'>
-            <li>Please download the Fuse wallet from Google play/Apple <br />app store by clicking on this <a target='_blank' rel='noopener noreferrer' href='http://fuseio.app.link/PKSgcxA6KZ'>link</a>
-              <div className='qr-code__download grid-x align-middle'>
-                <a href='https://apps.apple.com/il/app/fuse-wallet/id1491783654' target='_blank' rel='noopener noreferrer'><img src={AppleDownload} /></a>
-                <a href='https://play.google.com/store/apps/details?id=io.fuse.fusecash' target='_blank'><img src={GoogleDownload} /></a>
-              </div>
-            </li>
-            <li>Sign up to the app</li>
-            <li>Open the left menu and choose "switch community"</li>
-            <li>Scan this QR code to switch to the community you created!</li>
-          </ol>
-          <div className='qr-code__image'>
-            <QRCode size={150} value={value} />
+          <div className='qr-code__main'>
+            <ol className='qr-code__steps'>
+              <li>Please download the Fuse wallet from Google play/Apple <br />app store by clicking on this <a target='_blank' rel='noopener noreferrer' href='http://fuseio.app.link/PKSgcxA6KZ'>link</a>
+                <div className='qr-code__download grid-x align-middle'>
+                  <a href='https://apps.apple.com/il/app/fuse-wallet/id1491783654' target='_blank' rel='noopener noreferrer'><img src={AppleDownload} /></a>
+                  <a href='https://play.google.com/store/apps/details?id=io.fuse.fusecash' target='_blank'><img src={GoogleDownload} /></a>
+                </div>
+              </li>
+              <li>Sign up to the app</li>
+              <li>Open the left menu and choose "switch community"</li>
+              <li>Scan this QR code to switch to the community you created!</li>
+            </ol>
+            <div className='qr-code__image'>
+              <QRCode size={150} value={value} />
+            </div>
+          </div>
+          <div className='qr-code__desc'>
+            The Fuse wallet is open source and white label - more technical information on our <a target='_blank' href='https://github.com/fuseio/fuse-wallet'>Github</a>
           </div>
         </div>
       </div>
