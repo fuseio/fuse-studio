fbabab0230b8bb55548d11fd0666f9cb029ad44b
Leon Prouger <leonprou@gmail.com>
2020-03-05 12:05:47 +0200

    Fixing roost

--- server/src/services/firebase.js
+++ server/src/services/firebase.js
@@ -2,16 +2,18 @@ const admin = require('firebase-admin')
 const AWS = require('aws-sdk')
 const config = require('config')
 
+let roostAdmin
+
 const initAdmin = async () => {
   const secretsClient = new AWS.SecretsManager(config.aws.secrets.manager)
   const { SecretString } = await secretsClient.getSecretValue({ SecretId: config.aws.secrets.firebaseSecretId }).promise()
   admin.initializeApp({
     credential: admin.credential.cert(JSON.parse(SecretString))
   })
-  const { RoostSecretString } = await secretsClient.getSecretValue({ SecretId: config.aws.secrets.firebaseSecretIdRoost }).promise()
+  const response = await secretsClient.getSecretValue({ SecretId: config.aws.secrets.firebaseSecretIdRoost }).promise()
   roostAdmin = admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse(RoostSecretString))
-  }, "Roost")
+    credential: admin.credential.cert(JSON.parse(response.SecretString))
+  }, 'Roost')
 }
 
 initAdmin()
