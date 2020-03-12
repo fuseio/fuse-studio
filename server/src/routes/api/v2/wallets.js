5a6626e2e8d211ce6ac0e24bf00a0e9ac09f9ee8
Lior Rabin <lior9586@gmail.com>
2020-03-12 16:15:04 +0200

    lint (#246)

--- server/src/routes/api/v2/wallets.js
+++ server/src/routes/api/v2/wallets.js
@@ -45,7 +45,7 @@ router.post('/', auth.required, async (req, res, next) => {
         networks: ['fuse'],
         identifier
       }).save()
-      const job = await agenda.now('createWallet', { owner: accountAddress, correlationId, _id: userWallet._id})
+      const job = await agenda.now('createWallet', { owner: accountAddress, correlationId, _id: userWallet._id })
       return res.json({ job: job.attrs })
     }
   }
