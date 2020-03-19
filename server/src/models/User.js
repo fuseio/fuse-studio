fcd484715e70c8b947d12a81011929490f0556ab
Leon Prouger <leonprou@gmail.com>
2020-03-19 14:54:39 +0200

    Storing user names in DB

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -6,7 +6,8 @@ const UserSchema = new Schema({
   email: { type: String, required: [true, "can't be blank"], validate: [ validator.isEmail, 'invalid email' ] },
   accountAddress: { type: String, required: [true, "can't be blank"] },
   provider: { type: String, required: [true, "can't be blank"] },
-  source: { type: String, required: true }
+  source: { type: String, required: true },
+  displayName: { type: String }
 }, { timestamps: true })
 
 UserSchema.index({ email: 1, accountAddress: 1 }, { unique: true })
