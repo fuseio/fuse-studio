a016140d93ff2c4b5fd01cd5fa1997d71336e8e7
Leon Prouger <leonprou@gmail.com>
2020-03-25 16:40:43 +0200

    Fixing passing big numbers (#260)

--- dapp/src/utils/format.js
+++ dapp/src/utils/format.js
@@ -6,7 +6,7 @@ export const MAX_PRECISION = 18
 export const formatWei = (value, round = ROUND_PRECISION, decimals = 18) => new BigNumber(value).div(10 ** decimals).toFormat(round, BigNumber.ROUND_HALF_UP)
 export const formatEther = (value, round = ROUND_PRECISION) => new BigNumber(value).toFormat(round, BigNumber.ROUND_HALF_UP)
 
-export const toWei = (value, decimals = 18) => new BigNumber(value).multipliedBy(10 ** decimals).toString()
+export const toWei = (value, decimals = 18) => new BigNumber(value).multipliedBy(10 ** decimals).toFixed()
 
 const pickLetters = (word, numOfLetters) => word.substr(0, numOfLetters).toUpperCase()
 
