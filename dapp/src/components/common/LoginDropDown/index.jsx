56cdb290f1b0e6c0b7ba282f10be32d28907418f
Leon Prouger <leonprou@gmail.com>
2020-04-07 19:44:34 +0300

    Feature/add login (#268)
    * init
    
    * Adding login with google
    
    * fixing login endpoint
    
    * use google auth
    
    * Adding basic UI
    
    * new UI fixes
    
    * client id for QA and PROD
    
    * Adding disabled status to login button

---
+++ dapp/src/components/common/LoginDropDown/index.jsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { connect, useDispatch } from 'react-redux'
+import { push } from 'connected-react-router'
+import classNames from 'classnames'
+
+import { withAccount } from 'containers/Web3'
+
+import { getBalances, getProviderInfo, getCommunitiesKeys } from 'selectors/accounts'
+import { getCurrentNetworkType } from 'selectors/network'
+import { changeNetwork } from 'actions/network'
+import { loadModal } from 'actions/ui'
+import { fetchBalances } from 'actions/accounts'
+
+import GoogleLogin from 'react-google-login'
+import GoogleIcon from 'images/google.svg'
+
+import { login } from 'actions/user'
+
+const { clientId } = CONFIG.api.auth.google
+
+const LoginDropDown = () => {
+  const dispatch = useDispatch()
+
+  const renderButton = ({ onClick, disabled }) => {
+    return <div>
+      <button className={classNames({ 'login-modal__button': true, 'login-modal__disabled': disabled })} onClick={onClick} disabled={disabled}>
+        <img src={GoogleIcon} />
+        <div>Sign in  with Google</div>
+      </button>
+    </div>
+  }
+
+  const handleLogin = ({ tokenId }) => {
+    dispatch(login(tokenId))
+  }
+
+  return (
+    <div className='login-modal'>
+      <div className='login-modal__title'>Join Fuse</div>
+      <GoogleLogin
+        clientId={clientId}
+        render={renderButton}
+        onSuccess={handleLogin}
+        cookiePolicy={'single_host_origin'}
+      />
+      <div className='login-modal__bottom'>
+        Don't have a Google account? <br />
+        <a href='https://accounts.google.com/signup/v2' target='_blank'>Click here to create one</a>
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  communitiesKeys: getCommunitiesKeys(state),
+  providerInfo: getProviderInfo(state),
+  tokens: state.entities.tokens,
+  metadata: state.entities.metadata,
+  communities: state.entities.communities,
+  networkType: getCurrentNetworkType(state),
+  balances: getBalances(state)
+})
+
+const mapDispatchToProps = {
+  fetchBalances,
+  changeNetwork,
+  loadModal,
+  push
+}
+
+export default withAccount(connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(LoginDropDown))
--- dapp/src/containers/Root.jsx
+++ dapp/src/containers/Root.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Switch, Route } from 'react-router'
 
