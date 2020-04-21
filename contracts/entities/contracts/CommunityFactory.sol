66cda5ca6a3dbe6d25f95e8672a7c7e1c71b6d60
Lior Rabin <lior9586@gmail.com>
2020-04-21 13:29:12 +0300

    Feature/add register token method (#274)
    * Add register token method
    
    * registering token method
    
    * Update production.json
    
    * Update production.json
    
    Co-authored-by: Leon Prouger <leonprou@gmail.com>

--- contracts/entities/contracts/CommunityFactory.sol
+++ contracts/entities/contracts/CommunityFactory.sol
@@ -4,6 +4,7 @@ import "./CommunityTransferManager.sol";
 
 contract CommunityFactory is Ownable {
   event CommunityCreated(address indexed community, string name);
+  event TokenRegistered(address indexed community, address token);
 
   function createCommunity(string _name, address _admin) public {
     require(bytes(_name).length != 0, "NAME_CANNOT_BE_EMPTY");
@@ -16,5 +17,9 @@ contract CommunityFactory is Ownable {
     Community community = Community(_community);
 
     emit CommunityCreated(address(community), community.name());
+  }
+
+  function registerToken(address _community, address _token) public onlyOwner {
+    emit TokenRegistered(_community, _token);
   }
 }
