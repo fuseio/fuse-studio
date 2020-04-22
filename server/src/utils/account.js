21b66d1c0013b10f4650ac885c7dcb51120e04e6
Lior Rabin <lior@fuse.io>
2020-04-22 13:14:04 +0300

    create communityAdmin account on successful google login (account address is part of jwt)

--- server/src/utils/account.js
+++ server/src/utils/account.js
@@ -1,13 +1,16 @@
+const config = require('config')
 const mongoose = require('mongoose')
 const Account = mongoose.model('Account')
 var { fromMasterSeed } = require('ethereumjs-wallet/hdkey')
+const wallet = fromMasterSeed(config.get('secrets.accounts.seed'))
 
 const lockAccount = async (query = { role: '*' }) => {
   return Account.findOneAndUpdate({ isLocked: false, ...query }, { isLocked: true, lockingTime: new Date() })
 }
 
-const unlockAccount = async (address) =>
+const unlockAccount = async (address) => {
   Account.findOneAndUpdate({ address }, { isLocked: false, lockingTime: null })
+}
 
 const withAccount = (func, getAccount) => async (...params) => {
   const account = getAccount ? await getAccount(...params) : await lockAccount()
@@ -22,18 +25,23 @@ const withAccount = (func, getAccount) => async (...params) => {
     throw e
   }
 }
-const generateAccounts = (seed, accountsNumber) => {
-  const wallet = fromMasterSeed(seed)
-  for (let i = 0; i < accountsNumber; i++) {
-    const derivedWallet = wallet.deriveChild(i).getWallet()
-    const derivedAddress = derivedWallet.getChecksumAddressString()
-    console.log(derivedAddress)
-  }
+
+const createAccountWithRole = async (role) => {
+  const { childIndex } = await Account.findOne({}, { childIndex: 1 }).sort({ childIndex: -1 })
+  const newAccountChildIndex = childIndex + 1
+  const newAccountAddress = wallet.deriveChild(newAccountChildIndex).getWallet().getAddressString()
+  const account = await new Account({
+    address: newAccountAddress,
+    childIndex: newAccountChildIndex,
+    nonces: { home: 0, foreign: 0 },
+    role
+  }).save()
+  return { accountId: account._id, accountAddress: account.address }
 }
 
 module.exports = {
   lockAccount,
   unlockAccount,
   withAccount,
-  generateAccounts
+  createAccountWithRole
 }
