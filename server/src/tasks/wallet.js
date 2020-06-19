39210213c5fe425db142b9afe864e94e3880abed
Leon Prouger <leonprou@gmail.com>
2020-06-19 06:56:02 +0300

    Adding logs for blocknative watch address (#329)

--- server/src/tasks/wallet.js
+++ server/src/tasks/wallet.js
@@ -138,11 +138,14 @@ const createForeignWallet = withAccount(async (account, { userWallet, ens = '' }
   await UserWallet.findOneAndUpdate({ walletAddress }, { networks: userWallet.networks })
 
   try {
+    console.log(`Adding the address ${walletAddress} to the watch list of blocknative`)
     const response = await watchAddress(walletAddress)
     if (response.msg !== 'success') {
+      console.error(`Failed to the add the address ${walletAddress} to the watch list of blocknative`)
       throw new Error(response.msg ? response.msg : response)
     }
   } catch (e) {
+    console.error(`Failed to the add the address ${walletAddress} to the watch list of blocknative`)
     console.error(e)
   }
 
