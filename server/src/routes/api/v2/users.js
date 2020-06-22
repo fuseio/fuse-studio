10f76a0e9be0581a95955d8a7afc94b11d834a31
Leon Prouger <leonprou@gmail.com>
2020-06-22 17:39:15 +0300

    save

--- server/src/routes/api/v2/users.js
+++ server/src/routes/api/v2/users.js
@@ -1,13 +1,19 @@
 const router = require('express').Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
-const sendgridUtils = require('@utils/sendgrid')
+const { subscribeUser, notifyManagers} = require('@utils/sendgrid')
 
 router.post('/', async (req, res) => {
   const user = await new User(req.body).save()
 
   if (req.body.subscribe) {
-    sendgridUtils.subscribeUser(user)
+    subscribeUser(user)
+  }
+
+  if (req.body.source === 'studio') {
+    console.log('notify managers')
+    // sendgridUtils.notifyManagers({ communityName: formData.communityName, networkType: config.get('network.foreign.name') })
+
   }
 
   res.json({
