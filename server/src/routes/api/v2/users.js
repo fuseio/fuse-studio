de63e0fcd1730f897f10e8fb136b07f308c1dce8
Leon Prouger <leonprou@gmail.com>
2020-06-24 12:19:24 +0300

    reverts users api changes

--- server/src/routes/api/v2/users.js
+++ server/src/routes/api/v2/users.js
@@ -1,19 +1,13 @@
 const router = require('express').Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
-const { subscribeUser, notifyManagers} = require('@utils/sendgrid')
+const sendgridUtils = require('@utils/sendgrid')
 
 router.post('/', async (req, res) => {
   const user = await new User(req.body).save()
 
   if (req.body.subscribe) {
-    subscribeUser(user)
-  }
-
-  if (req.body.source === 'studio') {
-    console.log('notify managers')
-    // sendgridUtils.notifyManagers({ communityName: formData.communityName, networkType: config.get('network.foreign.name') })
-
+    sendgridUtils.subscribeUser(user)
   }
 
   res.json({
