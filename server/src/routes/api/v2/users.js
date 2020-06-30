bcde3428f441221f3179fe51f09b9ec2785be3dc
Leon Prouger <leonprou@gmail.com>
2020-06-30 14:59:42 +0300

    Fix to networks and users endpoint (#349)

--- server/src/routes/api/v2/users.js
+++ server/src/routes/api/v2/users.js
@@ -4,15 +4,23 @@ const User = mongoose.model('User')
 const sendgridUtils = require('@utils/sendgrid')
 
 router.post('/', async (req, res) => {
-  const user = await new User(req.body).save()
+  try {
+    const user = await new User(req.body).save()
 
-  if (req.body.subscribe) {
-    sendgridUtils.subscribeUser(user)
+    if (req.body.subscribe) {
+      sendgridUtils.subscribeUser(user)
+    }
+
+    res.json({
+      data: user
+    })
+  } catch (error) {
+    console.log(error.message || error)
+    res.json({
+      message: error.message,
+      error: error
+    })
   }
-
-  res.json({
-    data: user
-  })
 })
 
 router.post('/getnames', async (req, res) => {
