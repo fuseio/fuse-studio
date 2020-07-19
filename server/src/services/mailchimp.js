531e92e7e0bc77df6b632d85fc4cb8b0fcec3da5
Lior Agnin <Lior.Agnin@gmail.com>
2020-07-19 10:20:12 +0300

    oops (#357)
    Co-authored-by: Lior <liorag@colu.com>

--- server/src/services/mailchimp.js
+++ server/src/services/mailchimp.js
@@ -1,4 +1,4 @@
-const Mailchimp = require('mailchimp-api-v3t')
+const Mailchimp = require('mailchimp-api-v3')
 const config = require('config')
 
 const mailchimp = new Mailchimp(config.get('mail.mailchimp.apiKey'))
