8ff191b87e7ac8d2ed2884fab73ff4f24d0ffed9
Lior Agnin <Lior.Agnin@gmail.com>
2020-09-15 13:43:18 +0300

    added api - update user profile (#411)

--- server/src/routes/api/v2/users.js
+++ server/src/routes/api/v2/users.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
 const mailchimpUtils = require('@utils/mailchimp')
+const auth = require('@routes/auth')
 
 router.post('/', async (req, res) => {
   try {
@@ -14,6 +15,36 @@ router.post('/', async (req, res) => {
     res.json({
       data: user
     })
+  } catch (error) {
+    console.log(error.message || error)
+    res.json({
+      message: error.message,
+      error: error
+    })
+  }
+})
+
+router.put('/:accountAddress/avatar', auth.required, async (req, res) => {
+  try {
+    const { accountAddress } = req.params
+    const { avatarHash } = req.body
+    const user = await User.findOneAndUpdate({ accountAddress }, { avatarHash })
+    res.send({ data: user })
+  } catch (error) {
+    console.log(error.message || error)
+    res.json({
+      message: error.message,
+      error: error
+    })
+  }
+})
+
+router.put('/:accountAddress/name', auth.required, async (req, res) => {
+  try {
+    const { accountAddress } = req.params
+    const { displayName } = req.body
+    const user = await User.findOneAndUpdate({ accountAddress }, { displayName })
+    res.send({ data: user })
   } catch (error) {
     console.log(error.message || error)
     res.json({
