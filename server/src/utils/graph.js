8de9e9e0645f32197305282aa9d37f8b01e2b794
Lior Agnin <Lior.Agnin@gmail.com>
2020-09-23 15:25:18 +0300

    Hotfix graph (#428)
    * add logs
    
    * fixed
    
    * oops

--- server/src/utils/graph.js
+++ server/src/utils/graph.js
@@ -1,25 +1,29 @@
 const config = require('config')
 const mongoose = require('mongoose')
-const lodash = require('lodash')
-const web3Utils = require('web3-utils')
+const { isEmpty } = require('lodash')
+const { toChecksumAddress } = require('web3-utils')
 const { GraphQLClient } = require('graphql-request')
 const Community = mongoose.model('Community')
-const { toShortName } = require('@utils/network')
 
-const graphBridgeSubgraphClient = new GraphQLClient(`${config.get('graph.url')}${config.get(`graph.subgraphs.bridge${lodash.capitalize(toShortName(config.get(`network.foreign.name`)))}`)}`)
-const graphClient = new GraphQLClient(`${config.get('graph.url')}${config.get('graph.subgraphs.entities')}`)
+const graphEthereumBridgeClient = new GraphQLClient(`${config.get('graph.url')}${config.get(`graph.subgraphs.bridgeMain`)}`)
+const graphRopstenBridgeClient = new GraphQLClient(`${config.get('graph.url')}${config.get(`graph.subgraphs.bridgeRopsten`)}`)
+const graphFuseEntitiesClient = new GraphQLClient(`${config.get('graph.url')}${config.get('graph.subgraphs.entities')}`)
 
 export const fetchTokenByCommunity = async (communityAddress) => {
-  const community = await Community.findOne({ communityAddress: web3Utils.toChecksumAddress(communityAddress) }).lean()
+  const community = await Community.findOne({ communityAddress: toChecksumAddress(communityAddress) }).lean()
   if (community.isMultiBridge) {
     const { foreignTokenAddress } = community
     const query = `{bridgedTokens(where: {foreignAddress: "${foreignTokenAddress}"}) {address, name}}`
-    const { bridgedTokens } = await graphBridgeSubgraphClient.request(query)
+    const [bridgedTokensEthereum, bridgedTokensRopsten] = await Promise.all([
+      graphEthereumBridgeClient.request(query),
+      graphRopstenBridgeClient.request(query)
+    ])
+    const bridgedTokens = isEmpty(bridgedTokensRopsten) ? bridgedTokensEthereum : bridgedTokensRopsten
     console.log(`fetch token by community tokenAddress - ${bridgedTokens[0]['address']}`)
     return bridgedTokens[0]
   }
   const query = `{tokens(where: {communityAddress: "${communityAddress}"}) {address, communityAddress, originNetwork}}`
-  const { tokens } = await graphClient.request(query)
+  const { tokens } = await graphFuseEntitiesClient.request(query)
   console.log(`fetch token by community tokenAddress - ${tokens[0]['address']}`)
   return tokens[0]
 }
