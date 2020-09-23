b5d646e503ee308fb28e85186d67e56f6d8c501c
Lior <lior.agnin@fuse.io>
2020-09-23 13:31:12 +0300

    oops

--- server/src/utils/graph.js
+++ server/src/utils/graph.js
@@ -9,7 +9,7 @@ const graphBridgeSubgraphClient = new GraphQLClient(`${config.get('graph.url')}$
 const graphClient = new GraphQLClient(`${config.get('graph.url')}${config.get('graph.subgraphs.entities')}`)
 
 export const fetchTokenByCommunity = async (communityAddress) => {
-  const community = await Community.findOne({ communityAddress: web3Utils.toChecksumAddress(communityAddress) })
+  const community = await Community.findOne({ communityAddress: web3Utils.toChecksumAddress(communityAddress) }).lean()
   if (community.isMultiBridge) {
     const { foreignTokenAddress } = community
     const query = `{bridgedTokens(where: {foreignAddress: "${foreignTokenAddress}"}) {address}}`
