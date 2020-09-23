da3bccc0b958fc18dbeec8731a107358fc44a4c0
Lior <lior.agnin@fuse.io>
2020-09-23 15:21:46 +0300

    fixed

--- server/src/utils/graph.js
+++ server/src/utils/graph.js
@@ -1,14 +1,12 @@
 const config = require('config')
 const mongoose = require('mongoose')
-const lodash = require('lodash')
+const { isEmpty } = require('lodash')
 const { toChecksumAddress } = require('web3-utils')
 const { GraphQLClient } = require('graphql-request')
 const Community = mongoose.model('Community')
-const { toShortName } = require('@utils/network')
 
-const bridgeSubgraph = `bridge${lodash.capitalize(toShortName(config.get(`network.foreign.name`)))}`
-
-const graphBridgeSubgraphClient = new GraphQLClient(`${config.get('graph.url')}${config.get(`graph.subgraphs[${bridgeSubgraph}]`)}`)
+const graphEthereumBridgeClient = new GraphQLClient(`${config.get('graph.url')}${config.get(`graph.subgraphs.bridgeRopsten`)}`)
+const graphRopstenBridgeClient = new GraphQLClient(`${config.get('graph.url')}${config.get(`graph.subgraphs.bridgeRopsten`)}`)
 const graphFuseEntitiesClient = new GraphQLClient(`${config.get('graph.url')}${config.get('graph.subgraphs.entities')}`)
 
 export const fetchTokenByCommunity = async (communityAddress) => {
@@ -16,8 +14,11 @@ export const fetchTokenByCommunity = async (communityAddress) => {
   if (community.isMultiBridge) {
     const { foreignTokenAddress } = community
     const query = `{bridgedTokens(where: {foreignAddress: "${foreignTokenAddress}"}) {address, name}}`
-    console.log(`bridgeSubgraph query ${query}, graph url ${config.get('graph.url')}${config.get(`graph.subgraphs[${bridgeSubgraph}]`)}`)
-    const { bridgedTokens } = await graphBridgeSubgraphClient.request(query)
+    const [bridgedTokensEthereum, bridgedTokensRopsten] = await Promise.all([
+      graphEthereumBridgeClient.request(query),
+      graphRopstenBridgeClient.request(query)
+    ])
+    const bridgedTokens = isEmpty(bridgedTokensRopsten) ? bridgedTokensEthereum : bridgedTokensRopsten
     console.log(`fetch token by community tokenAddress - ${bridgedTokens[0]['address']}`)
     return bridgedTokens[0]
   }
