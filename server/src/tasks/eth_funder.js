9e87846590f774fea6d329141b712469c7fc3b4c
Lior <lior.agnin@fuse.io>
2020-11-02 10:44:50 +0200

    fixed

--- server/src/tasks/eth_funder.js
+++ server/src/tasks/eth_funder.js
@@ -11,35 +11,40 @@ const ethFunder = withAccount(async (account, { accountAddress, networkName }, j
   const bridgeType = networkName === 'fuse' ? 'home' : 'foreign'
   const network = createNetwork(bridgeType, account)
   const { web3, networkType } = network
-  const ethFunding = await new EthFunding({
-    accountAddress: toChecksumAddress(accountAddress),
-    fundingDate: new Date(),
-    network: networkType
-  }).save()
-  web3.eth.transactionConfirmationBlocks = parseInt(config.get(`network.foreign.contract.options.transactionConfirmationBlocks`))
-  const bonus = networkName
-    ? config.get('bonus.trade.fuse').toString()
-    : config.get('bonus.eth.ropsten').toString()
-  const receipt = await web3.eth.sendTransaction({
-    to: accountAddress,
-    from: account.address,
-    value: toWei(bonus),
-    nonce: account.nonces[bridgeType],
-    gasPrice: '1000000000',
-    gas: config.get('gasLimitForTx.funder')
-  }).on('transactionHash', (hash) => {
-    job.attrs.data.txHash = hash
-    job.save()
-  })
-  if (receipt) {
-    job.attrs.data.receipt = true
-    account.nonces[bridgeType]++
-    await setFunded(ethFunding._id)
-    await account.save()
-    job.save()
+  if (networkType === 'fuse' || networkType === 'ropsten') {
+    const ethFunding = await new EthFunding({
+      accountAddress: toChecksumAddress(accountAddress),
+      fundingDate: new Date(),
+      network: networkType
+    }).save()
+    web3.eth.transactionConfirmationBlocks = parseInt(config.get(`network.foreign.contract.options.transactionConfirmationBlocks`))
+    const bonus = networkName
+      ? config.get('bonus.trade.fuse').toString()
+      : config.get('bonus.eth.ropsten').toString()
+    const receipt = await web3.eth.sendTransaction({
+      to: accountAddress,
+      from: account.address,
+      value: toWei(bonus),
+      nonce: account.nonces[bridgeType],
+      gasPrice: '1000000000',
+      gas: config.get('gasLimitForTx.funder')
+    }).on('transactionHash', (hash) => {
+      job.attrs.data.txHash = hash
+      job.save()
+    })
+    if (receipt) {
+      job.attrs.data.receipt = true
+      account.nonces[bridgeType]++
+      await setFunded(ethFunding._id)
+      await account.save()
+      job.save()
+    }
+  } else {
+    throw Error(`Fund available only for ${networkName || 'ropsten'}`)
   }
 }, ({ networkName }) => {
-  return lockAccount({ role: networkName === 'fuse' ? '*' : 'eth' })
+  const bridgeType = networkName === 'fuse' ? 'home' : 'foreign'
+  return lockAccount({ role: 'eth', bridgeType })
 })
 
 module.exports = {
