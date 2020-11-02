ddcf4689ef7005d19370bcc910815bfe34460025
Leon Prouger <leonprou@gmail.com>
2020-11-02 11:57:09 +0200

    delete comment

--- server/src/routes/api/v1/fund.js
+++ server/src/routes/api/v1/fund.js
@@ -6,7 +6,6 @@ router.post('/:accountAddress', async (req, res, next) => {
   const { accountAddress } = req.params
   const networkName = req.body.networkName || 'ropsten'
 
-  // config.get('network.home.name')
   if (networkName !== 'fuse' && config.get('network.foreign.name') !== 'ropsten') {
     return res.status(404).json({ error: 'Funding available only for FUSE / Ropsten.' })
   }
