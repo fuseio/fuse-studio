cfe001c6a6d1ea8cf2c2add598e1055f2834995e
Leon Prouger <leonprou@gmail.com>
2020-11-17 23:09:48 +0200

    Fixing find one

--- server/src/services/taskManager.js
+++ server/src/services/taskManager.js
@@ -39,7 +39,7 @@ const startTask = async message => {
       } for task ${name} with task data ${JSON.stringify(taskData)}`
     )
 
-    const queueJob = await QueueJob.find({ messageId })
+    const queueJob = await QueueJob.findOne({ messageId })
     queueJob.accountAddress = account.address
     queueJob.status = 'started'
     await queueJob.save()
@@ -69,10 +69,11 @@ const startTask = async message => {
     return true
   } catch (err) {
     console.error(
-      `Unexpected rrror received in task ${name} with task data ${JSON.stringify(
+      `Unexpected error received in task ${name} with task data ${JSON.stringify(
         taskData
       )}, skipping. ${err}`
     )
+    console.log({ err })
     return false
   }
 }
