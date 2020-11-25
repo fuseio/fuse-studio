cd8f7a4125d4119eeff5ccd2ac95ddb8f08ea6c6
Leon Prouger <leonprou@gmail.com>
2020-11-25 13:57:08 +0200

    Add funding to V3

--- server/src/routes/api/v3/index.js
+++ server/src/routes/api/v3/index.js
@@ -3,4 +3,6 @@ const router = require('express').Router()
 router.use('/relay', require('./relay'))
 router.use('/wallets', require('./wallets'))
 router.use('/jobs', require('./jobs'))
+router.use('/fund', require('./fund'))
+
 module.exports = router
