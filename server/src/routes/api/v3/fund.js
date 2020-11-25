aff567f30504952ef06ecdc2131e86f0a3bf1679
Leon Prouger <leonprou@gmail.com>
2020-11-25 14:00:52 +0200

    Add funding to V3 (#512)

---
+++ server/src/routes/api/v3/fund.js
@@ -0,0 +1,17 @@
+const config = require('config')
+const router = require('express').Router()
+const taskManager = require('@services/taskManager')
+
+router.post('/:receiverAddress', async (req, res, next) => {
+  const { receiverAddress } = req.params
+  const networkName = req.body.networkName || 'ropsten'
+
+  if (networkName !== 'fuse' && config.get('network.foreign.name') !== 'ropsten') {
+    return res.status(404).json({ error: 'Funding available only for FUSE / Ropsten.' })
+  }
+
+  const job = await taskManager.now('ethFunder', { receiverAddress, bridgeType: networkName === 'fuse' ? 'home' : 'foreign', networkName })
+  return res.json({ job: job })
+})
+
+module.exports = router
