0032899f8079bf189be0a01bdf272607749ac508
Leon Prouger <leonprou@gmail.com>
2020-11-25 12:21:53 +0200

    Add API to create wallet on foreign (#511)
    * Add API to create wallet on foreign
    
    * fix docs
    
    * delete logs

--- server/src/routes/api/v2/admin/wallets.js
+++ server/src/routes/api/v2/admin/wallets.js
@@ -5,6 +5,7 @@ const { agenda } = require('@services/agenda')
 const auth = require('@routes/auth')
 const mongoose = require('mongoose')
 const UserWallet = mongoose.model('UserWallet')
+const { toChecksumAddress } = require('web3-utils')
 
 /**
  * @api {post} /api/v2/admin/wallets/create Create wallet for phone number
@@ -49,6 +50,40 @@ router.post('/create', auth.required, async (req, res) => {
   } catch (err) {
     return res.status(400).send({ error: err.message })
   }
+})
+
+
+/**
+ * @api {post} /api/v2/admin/wallets/create/foreign Create foreign wallet for the matching home
+ * @apiName CreateForeignWallet
+ * @apiGroup Admin
+ * @apiDescription Start async job of creating a wallet on the foreign network
+ *
+ * @apiExample Create wallet for the provided wallet address
+ *  POST /api/v2/admin/wallets/create/foreign
+ *  body: { wallerAddress: '0x92c358fcF6d270F97458C57583FCeabC086c3a26' }
+ *
+ * @apiParam {String} wallerAddress address create a wallet for, should be an existing wallet on home network (body parameter)
+ *
+ * @apiHeader {String} Authorization JWT Authorization in a format "Bearer {jwtToken}"
+ *
+ * @apiSuccess {String} Started job data
+ */
+router.post('/create/foreign', auth.admin, async (req, res) => {
+  const walletAddress = toChecksumAddress(req.body.walletAddress)
+  const userWallet = await UserWallet.findOne({ walletAddress })
+  const network = config.get('network.foreign.name')
+
+  if (userWallet.networks.includes(network)) {
+    return
+  }
+
+  const pendingNetworks = [...userWallet.pendingNetworks, network]
+  await UserWallet.findOneAndUpdate({ walletAddress }, { pendingNetworks })
+
+  console.log(`starting a createForeignWallet job for ${JSON.stringify({ walletAddress: userWallet.walletAddress, network })}`)
+  const job = await agenda.now('createForeignWallet', { userWallet, network: config.get('network.foreign.name') })
+  return res.json({ job: job.attrs })
 })
 
 module.exports = router
