2d60a042caae60ac0aa693f46f944889e6f754f1
Lior <lior.agnin@fuse.io>
2020-11-25 11:52:45 +0200

    oops

--- dapp/src/sagas/utils.js
+++ dapp/src/sagas/utils.js
@@ -60,7 +60,7 @@ export function * apiCall (apiFunc, params, options = {}) {
   if (options.v2) {
     apiRoot = CONFIG.api.v2.url
   } else if (options.v3) {
-    apiRoot = CONFIG.api.v2.url
+    apiRoot = CONFIG.api.v3.url
   } else {
     const networkType = (options && options.networkType) ? options.networkType : yield select(getForeignNetwork)
     apiRoot = getApiRoot(networkType)
