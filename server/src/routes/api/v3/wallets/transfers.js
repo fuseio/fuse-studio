cc74a185d901360040e8bc0c567038c3b9dfb106
Lior Agnin <Lior.Agnin@gmail.com>
2020-11-28 11:22:54 +0200

    removed pendingJobs from response (#516)

--- server/src/routes/api/v3/wallets/transfers.js
+++ server/src/routes/api/v3/wallets/transfers.js
@@ -6,19 +6,19 @@ const auth = require('@routes/auth')
 const request = require('request-promise-native')
 const { get, keyBy } = require('lodash')
 
-const formatPending = ({ _id, data: { transactionBody, txHash } }) => ({
-  jobId: _id,
-  value: get(transactionBody, 'value', 0),
-  tokenName: get(transactionBody, 'tokenName', ''),
-  tokenDecimal: get(transactionBody, 'tokenDecimal', ''),
-  tokenSymbol: get(transactionBody, 'asset', ''),
-  tokenAddress: get(transactionBody, 'tokenAddress', ''),
-  status: get(transactionBody, 'status', ''),
-  type: get(transactionBody, 'type', ''),
-  from: get(transactionBody, 'from', ''),
-  to: get(transactionBody, 'to', ''),
-  hash: txHash
-})
+// const formatPending = ({ _id, data: { transactionBody, txHash } }) => ({
+//   jobId: _id,
+//   value: get(transactionBody, 'value', 0),
+//   tokenName: get(transactionBody, 'tokenName', ''),
+//   tokenDecimal: get(transactionBody, 'tokenDecimal', ''),
+//   tokenSymbol: get(transactionBody, 'asset', ''),
+//   tokenAddress: get(transactionBody, 'tokenAddress', ''),
+//   status: get(transactionBody, 'status', ''),
+//   type: get(transactionBody, 'type', ''),
+//   from: get(transactionBody, 'from', ''),
+//   to: get(transactionBody, 'to', ''),
+//   hash: txHash
+// })
 
 const withJobs = async (transferEvents) => {
   const txHashesFromJobs = transferEvents.map(transferEvent => transferEvent.hash)
@@ -41,7 +41,7 @@ const withJobs = async (transferEvents) => {
  */
 router.get('/tokentx/:walletAddress', auth.required, async (req, res) => {
   const { walletAddress } = req.params
-  const { tokenAddress, sort = 'desc', startblock = 0, page = 1, offset = 50, skip, limit } = req.query
+  const { tokenAddress, sort = 'desc', startblock = 0, page = 1, offset = 50 } = req.query
   const responseTransferEvents = await request.get(`${config.get('explorer.fuse.urlBase')}?module=account&action=tokentx&contractaddress=${tokenAddress}&address=${walletAddress}&startblock=${startblock}&sort=${sort}&page=${page}&offset=${offset}`)
   const transferEvents = JSON.parse(responseTransferEvents)
 
@@ -58,16 +58,16 @@ router.get('/tokentx/:walletAddress', auth.required, async (req, res) => {
       }
     })
 
-    const pendingJobs = await QueueJob.find({
-      'data.walletAddress': walletAddress,
-      'data.transactionBody': { '$exists': true },
-      'data.transactionBody.status': 'pending',
-      'data.transactionBody.tokenAddress': tokenAddress.toLowerCase()
-    })
+    // const pendingJobs = await QueueJob.find({
+    //   'data.walletAddress': walletAddress,
+    //   'data.transactionBody': { '$exists': true },
+    //   'data.transactionBody.status': 'pending',
+    //   'data.transactionBody.tokenAddress': tokenAddress.toLowerCase()
+    // })
 
-    const formatPendingJobs = pendingJobs.filter(item => !result.some(({ hash }) => hash === item.hash)).map((jobInfo) => formatPending(jobInfo))
+    // const formatPendingJobs = pendingJobs.filter(item => !result.some(({ hash }) => hash === item.hash)).map((jobInfo) => formatPending(jobInfo))
 
-    return res.json({ data: [...result, ...formatPendingJobs] })
+    return res.json({ data: [...result] })
   }
 
   return res.json({ data: [] })
