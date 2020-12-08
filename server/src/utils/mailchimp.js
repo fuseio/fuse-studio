0be75bbdb03855068d74d8ca9dea0f895ca4c9be
Lior <lior.agnin@fuse.io>
2020-12-08 18:06:11 +0200

    added - tagging users

--- server/src/utils/mailchimp.js
+++ server/src/utils/mailchimp.js
@@ -1,11 +1,13 @@
 const mailchimp = require('@services/mailchimp')
 const config = require('config')
+// const { capitalize } = require('lodash')
 
-const subscribeUser = async (user) => {
+const subscribeUser = async (user, tags = ['Newsletter Sign Up']) => {
   const listId = config.get('mail.mailchimp.listId')
   const response = await mailchimp.post(`/lists/${listId}/members`, {
     email_address: user.email,
-    status: 'subscribed'
+    status: 'subscribed',
+    tags
   })
   if (response.statusCode >= 400) {
     throw Error(`Cannot add user ${user.email} to list`)
