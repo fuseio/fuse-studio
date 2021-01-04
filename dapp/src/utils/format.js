028f251f3f29b542f0bc28cd8b5ed1412aa6b8d7
Lior <lior.agnin@fuse.io>
2021-01-04 10:11:56 +0200

    UI fixes

--- dapp/src/utils/format.js
+++ dapp/src/utils/format.js
@@ -22,3 +22,5 @@ export const nameToSymbol = (name) => {
 }
 
 export const addressShortener = (address) => address ? `${address.substring(0, 6)}...${address.substring(address.length - 4, address.length)}` : ''
+
+export const formatNumber = (num) => String(num).replace(/(.)(?=(\d{3})+$)/g, '$1,')
