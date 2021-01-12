2d081bbae591129d124e05d50050e55d706098a8
Lior <lior.agnin@fuse.io>
2021-01-12 09:46:32 +0200

    fixed - can't reach bridge plugin

--- dapp/src/constants/plugins.js
+++ dapp/src/constants/plugins.js
@@ -72,111 +72,61 @@ const allPlugins = (isAdmin) => isAdmin
 
 export default allPlugins
 
-export const generalPlugins = (bridgeDirection = 'foreign-to-home') => {
-  return bridgeDirection === 'foreign-to-home'
-    ? [
-        {
-          title: 'Business list',
-          coverImage: BusinessList,
-          modalCoverPhoto: BusinessListBig,
-          key: 'businessList',
-          content: 'The business list allows to you integrate new businesses into your economy. Once a business is added they will appear in the mobile wallet and users will be able to transact with them freely.'
-        },
-        {
-          title: 'Wallet banner link',
-          coverImage: WalletBannerLink,
-          disabled: false,
-          modalCoverPhoto: WalletBannerLinkBig,
-          content: 'The wallet banner plug-in allows you to customize your wallet by adding an image and hyperlink of your choice to your wallet.',
-          key: 'walletBanner'
-        },
-        {
-          title: 'Bonuses',
-          coverImage: JoinBonus,
-          modalCoverPhoto: JoinBonusBig,
-          key: 'bonuses',
-          content: 'This plugin gives you the ability to reward users for completing multiple different actions. Join your economy, invite their friends or backup their wallet are all actions that can be rewarded with tokens.'
-        },
-        {
-          title: 'Fiat on ramp',
-          coverImage: FiatOnRamp,
-          disabled: false,
-          modalCoverPhoto: FiatOnRampBig,
-          content: `We have created integration with Moonpay, Transak, fiat gateways that allow you to embed the relevant payment option
-            (credit/debit cards and banks wires) inside your app and even show several option for your clients to choose from.
-            The integration allows your users to top their account without you holding any custody in the process! Here is how:
-            https://www.youtube.com/watch?v=ShxUIljvfLU&feature=emb_title`,
-          key: 'onramp'
-        },
-        {
-          title: 'Create Fuseswap pool',
-          coverImage: FuseswapPlugin,
-          disabled: false,
-          modalCoverPhoto: FuseswapPluginBig,
-          content: `Fuseswap is a decentralized exchange that allows anybody with tokens on Fuse to trade their tokens against a
-          smart contract that is managing a liquidity pool. It allows for users of the Studio that create a currency, to create a
-            liquidity pool on Fuseswap and let others trade this coin. In order to create the pool, please go to the following
-            link and add your token along with another currency so the pair can have an exchange rate. You can read more about
-              adding liquidity on the docs Please click here to set up your liquidity pool.`,
-          key: 'fuseswap'
-        }
-      ]
-    : [
-        {
-          title: 'Business list',
-          coverImage: BusinessList,
-          modalCoverPhoto: BusinessListBig,
-          key: 'businessList',
-          content: 'The business list allows to you integrate new businesses into your economy. Once a business is added they will appear in the mobile wallet and users will be able to transact with them freely.'
-        },
-        {
-          title: 'Wallet banner link',
-          coverImage: WalletBannerLink,
-          disabled: false,
-          modalCoverPhoto: WalletBannerLinkBig,
-          content: 'The wallet banner plug-in allows you to customize your wallet by adding an image and hyperlink of your choice to your wallet.',
-          key: 'walletBanner'
-        },
-        {
-          title: 'Bonuses',
-          coverImage: JoinBonus,
-          modalCoverPhoto: JoinBonusBig,
-          key: 'bonuses',
-          content: 'This plugin gives you the ability to reward users for completing multiple different actions. Join your economy, invite their friends or backup their wallet are all actions that can be rewarded with tokens.'
-        },
-        {
-          title: 'Fiat on ramp',
-          coverImage: FiatOnRamp,
-          disabled: false,
-          modalCoverPhoto: FiatOnRampBig,
-          content: `We have created integration with Moonpay, Transak, fiat gateways that allow you to embed the relevant payment option
-            (credit/debit cards and banks wires) inside your app and even show several option for your clients to choose from.
-            The integration allows your users to top their account without you holding any custody in the process! Here is how:
-            https://www.youtube.com/watch?v=ShxUIljvfLU&feature=emb_title`,
-          key: 'onramp'
-        },
-        {
-          title: 'Ethereum bridge',
-          coverImage: BridgePlugin,
-          disabled: false,
-          modalCoverPhoto: BridgePluginBig,
-          content: `We have created integration with Moonpay, Transak, fiat gateways that allow you to embed the relevant payment option
-            (credit/debit cards and banks wires) inside your app and even show several option for your clients to choose from.
-            The integration allows your users to top their account without you holding any custody in the process! Here is how:
-            https://www.youtube.com/watch?v=ShxUIljvfLU&feature=emb_title`,
-          key: 'bridge'
-        },
-        {
-          title: 'Create Fuseswap pool',
-          coverImage: FuseswapPlugin,
-          disabled: false,
-          modalCoverPhoto: FuseswapPluginBig,
-          content: `Fuseswap is a decentralized exchange that allows anybody with tokens on Fuse to trade their tokens against a
-          smart contract that is managing a liquidity pool. It allows for users of the Studio that create a currency, to create a
-            liquidity pool on Fuseswap and let others trade this coin. In order to create the pool, please go to the following
-            link and add your token along with another currency so the pair can have an exchange rate. You can read more about
-              adding liquidity on the docs Please click here to set up your liquidity pool.`,
-          key: 'fuseswap'
-        }
-      ]
-}
+export const generalPlugins = ([
+  {
+    title: 'Business list',
+    coverImage: BusinessList,
+    modalCoverPhoto: BusinessListBig,
+    key: 'businessList',
+    content: 'The business list allows to you integrate new businesses into your economy. Once a business is added they will appear in the mobile wallet and users will be able to transact with them freely.'
+  },
+  {
+    title: 'Wallet banner link',
+    coverImage: WalletBannerLink,
+    disabled: false,
+    modalCoverPhoto: WalletBannerLinkBig,
+    content: 'The wallet banner plug-in allows you to customize your wallet by adding an image and hyperlink of your choice to your wallet.',
+    key: 'walletBanner'
+  },
+  {
+    title: 'Bonuses',
+    coverImage: JoinBonus,
+    modalCoverPhoto: JoinBonusBig,
+    key: 'bonuses',
+    content: 'This plugin gives you the ability to reward users for completing multiple different actions. Join your economy, invite their friends or backup their wallet are all actions that can be rewarded with tokens.'
+  },
+  {
+    title: 'Fiat on ramp',
+    coverImage: FiatOnRamp,
+    disabled: false,
+    modalCoverPhoto: FiatOnRampBig,
+    content: `We have created integration with Moonpay, Transak, fiat gateways that allow you to embed the relevant payment option
+      (credit/debit cards and banks wires) inside your app and even show several option for your clients to choose from.
+      The integration allows your users to top their account without you holding any custody in the process! Here is how:
+      https://www.youtube.com/watch?v=ShxUIljvfLU&feature=emb_title`,
+    key: 'onramp'
+  },
+  {
+    title: 'Ethereum bridge',
+    coverImage: BridgePlugin,
+    disabled: false,
+    modalCoverPhoto: BridgePluginBig,
+    content: `We have created integration with Moonpay, Transak, fiat gateways that allow you to embed the relevant payment option
+      (credit/debit cards and banks wires) inside your app and even show several option for your clients to choose from.
+      The integration allows your users to top their account without you holding any custody in the process! Here is how:
+      https://www.youtube.com/watch?v=ShxUIljvfLU&feature=emb_title`,
+    key: 'bridge'
+  },
+  {
+    title: 'Create Fuseswap pool',
+    coverImage: FuseswapPlugin,
+    disabled: false,
+    modalCoverPhoto: FuseswapPluginBig,
+    content: `Fuseswap is a decentralized exchange that allows anybody with tokens on Fuse to trade their tokens against a
+     smart contract that is managing a liquidity pool. It allows for users of the Studio that create a currency, to create a
+      liquidity pool on Fuseswap and let others trade this coin. In order to create the pool, please go to the following
+       link and add your token along with another currency so the pair can have an exchange rate. You can read more about
+        adding liquidity on the docs Please click here to set up your liquidity pool.`,
+    key: 'fuseswap'
+  }
+])
