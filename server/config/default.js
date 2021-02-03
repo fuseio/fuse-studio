3a3a732dd44b7698602164009d7379cc63fcf9ab
Leon Prouger <leonprou@gmail.com>
2021-02-03 17:02:17 +0200

    Add FuseDollar token address

--- server/config/default.js
+++ server/config/default.js
@@ -84,7 +84,7 @@ module.exports = {
         CommunityFactory: '0xcc004Ee01Da7D9207aBE384cE601623E9F223536',
         MultiSigWallet: '0x0f5922B9c866c9d7de3E119c83a0A796A36A1307',
         TokenFactory: '0x78Cb0FeEC1fA3b3469cA18e79823b6A87C8929a4',
-        FuseDollar: '0x5D42867417d19D51A19661453A38B6F180F1624E',
+        FuseDollar: '0x249BE57637D8B013Ad64785404b24aeBaE9B098B',
         walletModules: {
           GuardianManager: '0xb2c9B85a41830655C0f21CAe43F552B6D76A709E',
           LockManager: '0xf8C62698F6D2322E04C8bDC386e7B640773715b7',
