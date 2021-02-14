e43cf9a0e8f443170f1cf964de1e2187c9cd7254
Leon Prouger <leonprou@gmail.com>
2021-02-14 13:50:48 +0200

    Hotfixinf getWalletModules

--- server/src/utils/wallet.js
+++ server/src/utils/wallet.js
@@ -1,12 +1,12 @@
 const config = require('config')
 const mongoose = require('mongoose')
 const Community = mongoose.model('Community')
-const { get } = require('lodash')
+const { get, has } = require('lodash')
 const homeAddresses = config.get('network.home.addresses')
 
 const getWalletModules = async (communityAddress) => {
   const community = await Community.findOne({ communityAddress })
-  if get(community, 'plugins.customWallet.isActive') {
+  if (has(community, 'plugins.customWallet.isActive')) {
     return get(community, 'plugins.customWallet.walletModules', homeAddresses.walletModules)
   } else {
     return homeAddresses.walletModules
