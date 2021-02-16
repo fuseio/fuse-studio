818d3e5a5c04666f101e9a565ce1cac83085c241
Lior <lior.agnin@fuse.io>
2021-02-16 14:30:05 +0200

    fixed receiverAddress

--- server/src/tasks/sqs/wallet/index.js
+++ server/src/tasks/sqs/wallet/index.js
@@ -64,7 +64,7 @@ const createWallet = async (account, { owner, communityAddress, phoneNumber, ens
     bonusInfo.bonusId = walletAddress
     if (isFunderDeprecated) {
       const { homeTokenAddress } = await Community.find({ communityAddress })
-      const bonusJob = await taskManager.now('fundToken', { phoneNumber, accountAddress: walletAddress, identifier: phoneNumber, tokenAddress: homeTokenAddress, communityAddress, bonusType: 'invite' }, { isWalletJob: true })
+      const bonusJob = await taskManager.now('fundToken', { phoneNumber, receiverAddress: walletAddress, identifier: phoneNumber, tokenAddress: homeTokenAddress, communityAddress, bonusType: 'invite' }, { isWalletJob: true })
       job.set('data.bonusJob', {
         name: bonusJob.name,
         _id: bonusJob._id.toString()
