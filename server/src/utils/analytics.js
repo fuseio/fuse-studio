6fd788c9aa4790bcd859868cfd91d19d7ca1d85b
Lior <lior.agnin@fuse.io>
2021-06-27 11:55:26 +0300

    wrap with try/catch

--- server/src/utils/analytics.js
+++ server/src/utils/analytics.js
@@ -5,11 +5,15 @@ const mongoose = require('mongoose')
 const UserWallet = mongoose.model('UserWallet')
 
 const trackDepositSuccess = async ({ walletAddress }) => {
-  const userWallet = await UserWallet.findOne({ walletAddress })
-  if (get(userWallet, 'os')) {
-    const { phoneNumber, os } = userWallet
-    const analyticsClient = new Analytics(config.get(`segment.${os}`))
-    analyticsClient.track({ event: 'fUSD Purchase Success', userId: phoneNumber })
+  try {
+    const userWallet = await UserWallet.findOne({ walletAddress })
+    if (get(userWallet, 'os')) {
+      const { phoneNumber, os } = userWallet
+      const analyticsClient = new Analytics(config.get(`segment.${os}`))
+      analyticsClient.track({ event: 'fUSD Purchase Success', userId: phoneNumber })
+    }
+  } catch (error) {
+    console.log(`Error while track deposit for ${walletAddress} ${error}`)
   }
 }
 
