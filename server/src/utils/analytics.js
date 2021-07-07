008eeb468ac7a54ff1b34a668cfb1c3db6320c59
Lior Agnin <Lior.Agnin@gmail.com>
2021-07-07 15:42:47 +0300

    minor fix (#690)

--- server/src/utils/analytics.js
+++ server/src/utils/analytics.js
@@ -1,19 +1,25 @@
 const Analytics = require('analytics-node')
 const config = require('config')
 const { get } = require('lodash')
+const { toChecksumAddress } = require('web3-utils')
 const mongoose = require('mongoose')
 const UserWallet = mongoose.model('UserWallet')
 
-const trackDepositSuccess = async ({ walletAddress }) => {
+const trackDepositSuccess = async ({ address }) => {
   try {
+    const walletAddress = toChecksumAddress(address)
     const userWallet = await UserWallet.findOne({ walletAddress })
+    console.log(`Called track deposit success walletAddress: ${walletAddress}`)
     if (get(userWallet, 'os')) {
       const { phoneNumber, os } = userWallet
       const analyticsClient = new Analytics(config.get(`segment.${os}`))
+      console.log(`fUSD Purchase Success: ${walletAddress} ${phoneNumber} ${os}`)
       analyticsClient.track({ event: 'fUSD Purchase Success', userId: phoneNumber })
+    } else {
+      console.log(`Couldn't track deposit success for ${walletAddress}`)
     }
   } catch (error) {
-    console.log(`Error while track deposit for ${walletAddress} ${error}`)
+    console.log(`Error while track deposit for ${address} ${error}`)
   }
 }
 
