a2e04bc3e579cf4c35a734b98eda654e65b205b5
Leon Prouger <leonprou@gmail.com>
2021-07-12 13:57:21 +0300

    Working on claim

--- server/src/routes/api/v2/wallets/apy.js
+++ server/src/routes/api/v2/wallets/apy.js
@@ -5,12 +5,26 @@ const auth = require('@routes/auth')
 const mongoose = require('mongoose')
 const RewardClaim = mongoose.model('RewardClaim')
 const { agenda } = require('@services/agenda')
+const UserWallet = require('../../../../models/WalletBalance')
 
 router.post('/claim/:walletAddress', auth.required, async (req, res) => {
+  const { walletAddress } = req.params
+  const { accountAddress } = req.user
+  const wallet = await UserWallet.findOne({ walletAddress })
+  if (wallet.accountAddress !== accountAddress) {
+    return res.status(403).json({ error: `account address ${accountAddress} does not own the wallet ${walletAddress}` })
+  }
+  const tokenAddress = config.get('network.home.addresses.FuseDollar')
+  let reward = await RewardClaim.findOne({ walletAddress, tokenAddress }).sort({ claimedAt: -1 })
+  if (reward.isClaimed) {
+    return res.status(400).json({ error: `no rewards are found for the wallet ${walletAddress}` })
+  } else {
+    
+  }
 
 })
 
-router.get('/reward/:walletAddress', async (req, res) => {
+router.get('/reward/:walletAddress', auth.required, async (req, res) => {
   const { walletAddress } = req.params
   const tokenAddress = config.get('network.home.addresses.FuseDollar')
   let reward = await RewardClaim.findOne({ walletAddress, tokenAddress, isClaimed: false }).sort({ claimedAt: -1 })
@@ -23,7 +37,7 @@ router.get('/reward/:walletAddress', async (req, res) => {
   if (reward) {
     return res.json({ data: { rewardAmount: reward } })
   } else {
-    return res.json({ error: `no reward found for wallet ${walletAddress}` })
+    return res.status(400).json({ error: `no reward found for wallet ${walletAddress}` })
   }
 })
 
