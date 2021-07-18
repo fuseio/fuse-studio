2f7f93ed7997d7afe5c2697daaa890cf27cdf81b
Lior <lior.agnin@fuse.io>
2021-07-18 16:36:57 +0300

    action for claimApy

--- server/src/routes/api/v2/wallets/transactions.js
+++ server/src/routes/api/v2/wallets/transactions.js
@@ -16,7 +16,7 @@ router.get('/', auth.required, async (req, res) => {
 router.get('/:walletAddress', auth.required, async (req, res) => {
   const { tokenAddress } = req.query
   const { walletAddress } = req.params
-  const { docs, hasNextPage } = await WalletTransaction.paginate(clean({ tokenAddress, $or: [ { to: walletAddress }, { from: walletAddress } ] }))
+  const { docs, hasNextPage } = await WalletTransaction.paginate(clean({ tokenAddress, $or: [{ to: walletAddress }, { from: walletAddress }] }))
 
   res.send({ data: docs, hasNextPage })
 })
