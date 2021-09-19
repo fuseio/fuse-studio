6d67ec554721c82e30f99c27722030bac7c59434
Leon Prouger <leonprou@gmail.com>
2021-09-19 20:39:12 +0300

    Fix to APY claim (#735)

--- server/src/tasks/sqs/apy.js
+++ server/src/tasks/sqs/apy.js
@@ -1,7 +1,7 @@
 const apy = require('@utils/apy')
 
-const claimApy = async (account, { walletAddress, tokenAddress }, job) => {
-  return apy.claimApy(account, { walletAddress, tokenAddress }, job)
+const claimApy = async (account, data, job) => {
+  return apy.claimApy(account, data, job)
 }
 
 module.exports = {
