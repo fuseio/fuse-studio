8d51a8084fb8e65a9909c2f6c74dcd0dd43aac2a
Maor Stamati <maorstamati@gmail.com>
2022-07-28 12:02:37 +0300

    Deleted unused import if isEmpty

--- server/src/routes/api/v2/admin/tokens.js
+++ server/src/routes/api/v2/admin/tokens.js
@@ -11,7 +11,6 @@ const QueueJob = mongoose.model('QueueJob')
 const request = require('request-promise-native')
 const Promise = require('bluebird')
 const { toChecksumAddress } = require('web3-utils')
-import isEmpty from 'lodash/isEmpty'
 
 /**
  * @api {post} /api/v2/admin/tokens/create Create token
