b3443018ddc6896a6461d6899b38de954a9c3db6
Lior <lior.agnin@fuse.io>
2023-07-12 11:17:09 +0300

    More fixes

--- server/src/services/mongo.js
+++ server/src/services/mongo.js
@@ -5,9 +5,7 @@ function start () {
   const config = require('config')
 
   mongoose.set('debug', config.get('mongo.debug'))
-  mongoose.set('useFindAndModify', false)
-  mongoose.set('useCreateIndex', true)
-
+  mongoose.set('strictQuery', false)
   mongoose.connect(config.get('mongo.uri'), config.get('mongo.options')).catch((error) => {
     console.error(error)
     process.exit(1)
